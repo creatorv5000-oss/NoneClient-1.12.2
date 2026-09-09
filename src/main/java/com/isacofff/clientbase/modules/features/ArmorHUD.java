f24f14bb07e3996692385ec7172b9f9d1ac07425
creatorv5000-oss <creatorv5000@gmail.com>
2026-09-09 16:42:51 -0500

    Update ArmorHUD.java

--- src/main/java/com/isacofff/clientbase/modules/features/ArmorHUD.java
+++ src/main/java/com/isacofff/clientbase/modules/features/ArmorHUD.java
@@ -2,19 +2,33 @@ package com.isacofff.clientbase.modules.features;
 
 import com.isacofff.clientbase.Category;
 import com.isacofff.clientbase.modules.Module;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
 
 public class ArmorHUD extends Module {
 
+    protected final Minecraft mc = Minecraft.getMinecraft();
+
     public ArmorHUD() {
         super(
-                "ArmorHUD",
-                "Displays your equipped armor",
-                Category.Render
+            "ArmorHUD",
+            "Displays your equipped armor",
+            Category.Render
         );
     }
 
     @Override
     public void onUpdate() {
-        // Armor HUD rendering will be connected to the HUD render hook later.
+        if (mc.player == null || mc.world == null) {
+            return;
+        }
+
+        for (int i = 0; i < mc.player.inventory.armorInventory.size(); i++) {
+            ItemStack armorStack = mc.player.inventory.armorInventory.get(i);
+            
+            if (!armorStack.isEmpty()) {
+                
+            }
+        }
     }
 }
