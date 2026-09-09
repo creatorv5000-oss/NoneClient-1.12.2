c7fc668ac04baba5838712f49d9e810987989610
creatorv5000-oss <creatorv5000@gmail.com>
2026-09-09 12:42:53 -0500

    Implement keystrokes rendering on screen

--- src/main/java/com/isacofff/clientbase/modules/features/Keystrokes.java
+++ src/main/java/com/isacofff/clientbase/modules/features/Keystrokes.java
@@ -2,8 +2,15 @@ package com.isacofff.clientbase.modules.features;
 
 import com.isacofff.clientbase.Category;
 import com.isacofff.clientbase.modules.Module;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.input.Keyboard;
 
 public class Keystrokes extends Module {
+
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     public Keystrokes() {
         super(
@@ -13,8 +20,37 @@ public class Keystrokes extends Module {
         );
     }
 
-    @Override
-    public void onUpdate() {
-        // HUD rendering will be added through the GUI/render hook.
+    public void render(ScaledResolution resolution) {
+        if (mc.player == null) {
+            return;
+        }
+
+        FontRenderer font = mc.fontRendererObj;
+
+        int x = 10;
+        int y = resolution.getScaledHeight() - 90;
+
+        int size = 24;
+        int gap = 2;
+
+        drawKey(font, "W", x + size + gap, y, Keyboard.isKeyDown(Keyboard.KEY_W));
+        drawKey(font, "A", x, y + size + gap, Keyboard.isKeyDown(Keyboard.KEY_A));
+        drawKey(font, "S", x + size + gap, y + size + gap, Keyboard.isKeyDown(Keyboard.KEY_S));
+        drawKey(font, "D", x + (size + gap) * 2, y + size + gap,
+                Keyboard.isKeyDown(Keyboard.KEY_D));
+    }
+
+    private void drawKey(FontRenderer font, String text, int x, int y, boolean pressed) {
+        int background = pressed ? 0xFF555555 : 0xAA222222;
+
+        GuiHelper.drawRect(x, y, x + 24, y + 24, background);
+
+        int textWidth = font.getStringWidth(text);
+        font.drawString(
+                text,
+                x + (24 - textWidth) / 2,
+                y + 8,
+                0xFFFFFFFF
+        );
     }
 }
