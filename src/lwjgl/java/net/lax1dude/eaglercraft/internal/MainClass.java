5ae7c61506feb3fc24aca8fb47da86d1be8b86da
creatorv5000-oss <creatorv5000@gmail.com>
2026-09-10 13:14:23 -0500

    Update MainClass.java

--- src/lwjgl/java/net/lax1dude/eaglercraft/internal/MainClass.java
+++ src/lwjgl/java/net/lax1dude/eaglercraft/internal/MainClass.java
@@ -2,8 +2,10 @@ package net.lax1dude.eaglercraft.internal;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-		LWJGLEntryPoint.main_(args);
-	}
-
+    public static void main(String[] args) {
+        // Activates the FPS optimization variables before launching the game engine
+        net.lax1dude.eaglercraft.FPSOptimizer.initializeEngineOverrides();
+        
+        LWJGVMEntryPoint.main_(args);
+    }
 }
